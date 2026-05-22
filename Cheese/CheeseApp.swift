f0c4ebd2c8cfe155cc7e8c0db01bc213001a336f
qugaa <boratassdemir@gmail.com>
2026-05-22 03:30:10 +0200

    Initial Commit

---
+++ Cheese/CheeseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheeseApp.swift
+//  Cheese
+//
+//  Created by Bora on 22/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct CheeseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cheese/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cheese
+//
+//  Created by Bora on 22/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
